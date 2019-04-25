82188ca032527afef76807b07cbec7b08d9c42ce
guangyingyuan <39292595+guangyingyuan@users.noreply.github.com>
2019-04-25 23:06:48 +0800

    Update helloworld.go
    fun2

--- helloworld.go
+++ helloworld.go
@@ -9,7 +9,7 @@ import (
 )
 
 func main() {
-	text := "demo fun1"
+	text := "demo fun2"
 
 	rev := os.Getenv("K_REVISION") // K_REVISION=helloworld-7vh75
 	if i := strings.LastIndex(rev, "-"); i > 0 {
